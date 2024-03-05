e521d24925c3aea99946c7dfbc8046a7a8c87a52
JUANESZz1 <159485717+JUANESZz1@users.noreply.github.com>
2024-03-04 22:07:50 -0500

    Correccion grupo

--- src/main/java/classroom/Grupo.java
+++ src/main/java/classroom/Grupo.java
@@ -5,7 +5,7 @@ public class Grupo {
     public Persona[] estudiantes;
     public Persona profesor;
     public Asignatura asignatura;
-    public final int codigo = 0;
+    public final int codigo;
     public String horario;
 
     public Grupo(Persona[] estudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
@@ -17,14 +17,19 @@ public class Grupo {
     }
 
     public Grupo(int cantidadEstudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
-        Persona[] personas = new Persona[cantidadEstudiantes];
-        this(personas, profesor, asignatura, codigo, horario);
+        this.estudiantes = new Persona[cantidadEstudiantes]; 
+        this.profesor = profesor;
+        this.asignatura = asignatura;
+        this.codigo = codigo;
+        this.horario = horario;
     }
 
     public Grupo(Persona[] estudiantes, Persona profesor, Asignatura asignatura) {
         this.estudiantes = estudiantes;
         this.profesor = profesor;
         this.asignatura = asignatura;
+        this.codigo = 0; 
+        this.horario = "";
     }
 
     void cambiarEstudiante(Persona estudianteViejo, Persona estudianteNuevo) {
@@ -35,8 +40,9 @@ public class Grupo {
             }
         }
     }
-    
+
     void cambiarEstudiante(int indice, Persona estudiante) {
         estudiantes[indice] = estudiante;
     }
 }
+
